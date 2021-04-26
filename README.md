dcf7d97f262a1210d52c3adca5f502c3dbe79f2b
Joe Plumb <hello@joeplumb.com>
2021-04-26 17:31:01 +0100

    adding notebooks and initial code for automated deployment

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # Azure Data Factory, Azure Machine Learning, and Terraform Example
 
-`tf -chdir=.cloud/dev/ init`
+## TODO: Architecture diagram
 
-`tf -chdir=.cloud/dev/ plan`
+## Prerequistes
+- Azure Subscription
+- `az` cli and `terraform` cli
+- Python environment with `azureml` sdk
 
-`tf -chdir=.cloud/dev/ apply`
+## Steps
+- Deploy infrastructure with Terraform 
+    - fork then clone this repo to your environment
+    - edit the `main.tf` to point to your cloned github repo for ADF development
+    - in the root dir run:
+        ```
+        tf -chdir=.cloud/dev/ init
+        tf -chdir=.cloud/dev/ plan
+        tf -chdir=.cloud/dev/ apply
+        ```
+    - Update adf artefacts by running `chmod u+x utils/adfupdate.sh %% ./utils/adfupdate.sh`
+    - Once updated, merge the changes into your `main` branch via PR
+- Deploy AML pipeline (interactive):
+    - Navigate to your ml workspace and download the `config.json` from the portal - [more details here](https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-environment#workspace)
+    - Open `notebooks/PipelineParameters.ipynb` and run through the cells. This will create and publish a pipeline into your AzureML workspace
+    - Navigate to Azure Data Factory and open the *AMLPipeline* in the Author tab.
+        - Update the Settings of the AzureML pipeline execution step to point to the published `Machine Learning Pipeline ID` (this will auto-populate from the dropdown)
+    - Save the changes, and test the integration by clicking *Debug*.
+
+## TODO
+- Automated deployment of AML pipeline via Azure DevOps
--- utils/adfupdate.sh
+++ utils/adfupdate.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # checkout adf-update branch
+echo "Creating new branch 'adf-objects-update' to automate and publish changes..."
 git checkout -b adf-objects-update
 
+echo "Creating directories in adf/ folder for objects..."
+mkdir adf/factory
+mkdir adf/linkedService
+
 # update local adf files 
+echo "Updating Key Vault definition from terraform output..."
 ## update key vault linked service defintion
 akv_baseUrl=$(terraform -chdir=.cloud/dev/ output -json | jq '.akv_baseUrl.value')
-jq --argjson v0 "$akv_baseUrl" '.typeProperties.baseUrl = $v0' utils/templates/AzureKeyVault1-template.json >| adf/linkedService/AzureKeyVault1.json
+jq --argjson v0 "$akv_baseUrl" '.properties.typeProperties.baseUrl = $v0' utils/templates/AzureKeyVault1-template.json > adf/linkedService/AzureKeyVault1.json
 
 ## update aml linked service definition
+echo "Updating AzureML definition from terraform output..."
 appId=$(terraform -chdir=.cloud/dev/ output -json | jq '.appId.value')
 mlWorkspaceName=$(terraform -chdir=.cloud/dev/ output -json | jq '.mlWorkspaceName.value')
 resourceGroupName=$(terraform -chdir=.cloud/dev/ output -json | jq '.resourceGroupName.value')
@@ -19,20 +26,17 @@ jq --argjson v1 "$appId" \
 --argjson v3 "$resourceGroupName" \
 --argjson v4 "$subscriptionId" \
 --argjson v5 "$tenant" \
-'.properties.typeProperties.appId = $v1 | .properties.typeProperties.mlWorkspaceName = $v2 | .properties.typeProperties.resourceGroupName = $v3 | .properties.typeProperties.subscriptionId = $v4 | .properties.typeProperties.tenant = $v5' utils/templates/AzureMLService1-template.json >| adf/linkedService/AzureMLService1.json
+'.properties.typeProperties.servicePrincipalId = $v1 | .properties.typeProperties.mlWorkspaceName = $v2 | .properties.typeProperties.resourceGroupName = $v3 | .properties.typeProperties.subscriptionId = $v4 | .properties.typeProperties.tenant = $v5' utils/templates/AzureMLService1-template.json > adf/linkedService/AzureMLService1.json
 
 ## update adf
+echo "Updating ADF definition from terraform output..."
 adfName=$(terraform -chdir=.cloud/dev/ output -json | jq '.adf_name.value')
 location=$(terraform -chdir=.cloud/dev/ output -json | jq '.location.value')
 env=$(terraform -chdir=.cloud/dev/ output -json | jq '.env.value')
 jq --argjson v1 "$adfName" \
 --argjson v2 "$env" \
 --argjson v3 "$location" \
-'.name = $v1 | .properties.globalParameters.environment.value = $v2 | .location = $v3 ' utils/templates/template.json >| adf/factory/${adfName//\"}-test.json
-
-rm adf/factory/template.json
-rm adf/linkedService/AzureKeyVault1-template.json
-rm adf/linkedService/AzureMLService1-template.json
+'.name = $v1 | .properties.globalParameters.environment.value = $v2 | .location = $v3 ' utils/templates/template.json > adf/factory/${adfName//\"}.json
 
 # commit and push changes to branch
 git add adf/
